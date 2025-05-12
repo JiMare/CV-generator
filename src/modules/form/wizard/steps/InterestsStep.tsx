e8cc2200094fce3b0634789ec0eb9c3fddd28e59
JiMare <zoa@seznam.cz>
2025-05-12 11:42:02 +0200

    finish section interests and soft skills

--- src/modules/form/wizard/steps/InterestsStep.tsx
+++ src/modules/form/wizard/steps/InterestsStep.tsx
@@ -1,15 +1,33 @@
 import { StepNavigator } from '@/components/StepNavigator';
+import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
+import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Controller, useFormContext } from 'react-hook-form';
+import { Textarea } from '@/components/ui/textarea';
+import { cn } from '@/lib/utils';
+import { TrashIcon } from 'lucide-react';
+import { Controller, useFieldArray, useFormContext, useWatch } from 'react-hook-form';
+import { Interest } from '../../FormContext';
 
 export const InterestsStep = () => {
   const {
     register,
     control,
     formState: { errors },
-    trigger,
   } = useFormContext();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'interests',
+  });
+
+  const interestsHidden = useWatch({ name: 'interestsHidden' });
+  const interests = useWatch({ control, name: 'interests' });
+
+  const handleAddInterest = () => {
+    append({ name: '', details: '' });
+  };
+
+  const isInterestsValid = interests.every((interest: Interest) => interest.name.trim() !== '');
   return (
     <div className="flex flex-col gap-6 h-full">
       <div className="flex items-baseline justify-between">
@@ -27,6 +45,45 @@ export const InterestsStep = () => {
           />
         </div>
       </div>
+      {!interestsHidden && (
+        <>
+          <div>
+            <Label>Heading</Label>
+            <Input type="text" {...register('interestsHeading')} />
+          </div>
+          <div className="flex flex-col gap-4 flex-grow overflow-y-auto">
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id} className="p-6 border rounded-lg flex flex-col gap-4 relative">
+                  <button type="button" onClick={() => remove(index)} className="absolute top-4 right-4 text-red-500">
+                    <TrashIcon className="h-4 w-4" />
+                  </button>
+                  <div>
+                    <Label>Name</Label>
+                    <Input
+                      {...register(`interests.${index}.name`, {
+                        validate: (value) => value.trim() !== '',
+                      })}
+                      placeholder="Enter hobby name"
+                      className={cn('border', Boolean((errors?.interests as any)?.[index]?.name) && 'border-red-500')}
+                    />
+                  </div>
+
+                  <div>
+                    <Label>Details</Label>
+                    <Textarea {...register(`interests.${index}.details`)} placeholder="Enter hobby details" />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="flex justify-end">
+            <Button type="button" onClick={handleAddInterest} disabled={!isInterestsValid}>
+              + Add Hobby
+            </Button>
+          </div>
+        </>
+      )}
       <StepNavigator toPrev="/wizard/soft-skills" />
     </div>
   );
--- src/modules/form/wizard/steps/SoftSkillsStep.tsx
+++ src/modules/form/wizard/steps/SoftSkillsStep.tsx
@@ -1,15 +1,33 @@
 import { StepNavigator } from '@/components/StepNavigator';
+import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
+import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Controller, useFormContext } from 'react-hook-form';
+import { Textarea } from '@/components/ui/textarea';
+import { cn } from '@/lib/utils';
+import { TrashIcon } from 'lucide-react';
+import { Controller, useFieldArray, useFormContext, useWatch } from 'react-hook-form';
+import { SoftSkill } from '../../FormContext';
 
 export const SoftSkillsStep = () => {
   const {
     register,
     control,
     formState: { errors },
-    trigger,
   } = useFormContext();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'softSkills',
+  });
+
+  const softSkillsHidden = useWatch({ name: 'softSkillsHidden' });
+  const softSkills = useWatch({ control, name: 'softSkills' });
+
+  const handleAddSoftSkill = () => {
+    append({ name: '', details: '' });
+  };
+
+  const isSoftSkillsValid = softSkills.every((skill: SoftSkill) => skill.name.trim() !== '');
   return (
     <div className="flex flex-col gap-6 h-full">
       <div className="flex items-baseline justify-between">
@@ -27,6 +45,45 @@ export const SoftSkillsStep = () => {
           />
         </div>
       </div>
+      {!softSkillsHidden && (
+        <>
+          <div>
+            <Label>Heading</Label>
+            <Input type="text" {...register('softSkillsHeading')} />
+          </div>
+          <div className="flex flex-col gap-4 flex-grow overflow-y-auto">
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id} className="p-6 border rounded-lg flex flex-col gap-4 relative">
+                  <button type="button" onClick={() => remove(index)} className="absolute top-4 right-4 text-red-500">
+                    <TrashIcon className="h-4 w-4" />
+                  </button>
+                  <div>
+                    <Label>Name</Label>
+                    <Input
+                      {...register(`softSkills.${index}.name`, {
+                        validate: (value) => value.trim() !== '',
+                      })}
+                      placeholder="Enter soft skill name"
+                      className={cn('border', Boolean((errors?.softSkills as any)?.[index]?.name) && 'border-red-500')}
+                    />
+                  </div>
+
+                  <div>
+                    <Label>Details</Label>
+                    <Textarea {...register(`softSkills.${index}.details`)} placeholder="Enter soft skill details" />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="flex justify-end">
+            <Button type="button" onClick={handleAddSoftSkill} disabled={!isSoftSkillsValid}>
+              + Add Soft skill
+            </Button>
+          </div>
+        </>
+      )}
       <StepNavigator toPrev="/wizard/languages" toNext="/wizard/interests" />
     </div>
   );
