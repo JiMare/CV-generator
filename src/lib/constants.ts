3475ec81f17f3d02bf5035d317b5e9c00d7079ed
JiMare <zoa@seznam.cz>
2025-07-10 22:28:53 +0200

    fix languages, remember other select value

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,15 +3,6 @@ export const githubRegex = /^https:\/\/(www\.)?github\.com\/.+$/i;
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const urlRegex = /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/\S*)?$/;
 
-export const cefrLenaguagesLevelsOptions = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
-export const linkedinLanguagesLevelsOptions = [
-  'Elementary proficiency',
-  'Limited working proficiency',
-  'Professional working proficiency',
-  'Full professional proficiency',
-  'Native or bilingual proficiency',
-];
-
 export const languageCefrOptions = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
 export const languageLinkedinOptions = [
   'Elementary proficiency',
--- src/modules/form/wizard/steps/langauges/LanguageProficiency.tsx
+++ src/modules/form/wizard/steps/langauges/LanguageProficiency.tsx
@@ -3,7 +3,7 @@ import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { languageCefrOptions, languageLinkedinOptions } from '@/lib/constants';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { ControllerRenderProps, FieldValues } from 'react-hook-form';
 
 type Props = {
@@ -11,14 +11,27 @@ type Props = {
 };
 
 export const LanguageProficiency = ({ field }: Props) => {
-  const [type, setType] = useState<'cefr' | 'linkedin' | 'custom'>('custom');
+  const languageType = languageCefrOptions.includes(field.value)
+    ? 'cefr'
+    : languageLinkedinOptions.includes(field.value)
+    ? 'linkedin'
+    : 'custom';
+  const [type, setType] = useState<'cefr' | 'linkedin' | 'custom'>(languageType);
+  const cefrLevelRef = useRef(languageType === 'cefr' ? field.value : '');
+  const linkedinLevelRef = useRef(languageType === 'linkedin' ? field.value : '');
 
   return (
     <div>
       <RadioGroup
         value={type}
         onValueChange={(val) => {
-          field.onChange('')
+          if (val === 'cefr') {
+            field.onChange(cefrLevelRef.current);
+          } else if (val === 'linkedin') {
+            field.onChange(linkedinLevelRef.current);
+          } else if (val === 'custom') {
+            field.onChange('');
+          }
           setType(val as any);
         }}
         className="flex flex-col gap-2"
@@ -32,6 +45,7 @@ export const LanguageProficiency = ({ field }: Props) => {
             <Select
               value={field.value}
               onValueChange={(level) => {
+                cefrLevelRef.current = level;
                 field.onChange(level);
               }}
             >
@@ -58,6 +72,7 @@ export const LanguageProficiency = ({ field }: Props) => {
             <Select
               value={field.value}
               onValueChange={(level) => {
+                linkedinLevelRef.current = level;
                 field.onChange(level);
               }}
             >
